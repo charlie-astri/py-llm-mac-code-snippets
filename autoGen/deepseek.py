830035ca7d8ef7a02f04493556db92d986458b4e
Charlie J.L. XU <charliexu@astri.org>
2024-05-28 15:21:06 +0800

    Add config for deepseek api

---
+++ autoGen/deepseek.py
@@ -0,0 +1,11 @@
+
+# llm-config setting for deepseek model
+# reference URL: https://platform.deepseek.com/
+llm_config = {
+    "config_list": [{
+        "model": "deepseek-chat",
+        "api_key": "you api key on deepseek platform",
+        "base_url": "https://api.deepseek.com",
+        "temperature": 0
+    }]
+}
